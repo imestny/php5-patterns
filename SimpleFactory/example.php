abee9d782ea7a3b83fe37bbc9c59d3a551f9bbc4
imestny <imestny@gmail.com>
2013-11-28 21:12:40 +0400

    Factory class is added.

--- SimpleFactory/example.php
+++ SimpleFactory/example.php
@@ -1,7 +1,35 @@
 <?php
 
-namespace php5-patterns\SimpleFactory;
+namespace phpDesignPatterns\SimpleFactory;
 
-class
+class Factory
+{
+    /**
+    * Храним список объектов в массиве, ключ - имя класса
+    *
+    * @var array
+    */
+    private static $instances = array();
+    
+        /**
+        * Вместо автоматической инициализации объектов - можно поставить CASE условие
+        * в котором будет ограниченное число классов и класс по умолчанию
+        */
+        public static function getInstance($className)
+        {
+            if (array_key_exists($className, self::$instances)) {
+                    return self::$instances[$className];
+            }
+            
+            return self::$instances[$className] = new $className();
+        }
+}
+
+/**
+ * Таким образом, инициализируем нужные объекты через одну метод.
+ */
+$hulk = Factory::getInstance('Hulk');
+$thor = Factory::getInstance('Thor');
+
 
 ?>
